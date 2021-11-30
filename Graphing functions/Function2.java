1a15aff691a52952e762e9af655297a11cdabc77
Sahaj Singh <89856591+Sahajsingh10@users.noreply.github.com>
2021-11-30 17:42:04 -0500

    Add files via upload

---
+++ Graphing functions/Function2.java
@@ -0,0 +1,16 @@
+package application;
+
+
+
+public class Function2 extends Function {
+
+	@Override
+	public double fnValue(double x) {
+		return Math.sin(x);
+	}
+	
+	public String toString() {
+		return "sin(x)";
+	}
+
+}
